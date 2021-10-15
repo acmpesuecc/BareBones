fde7e4cbdd54cb50cbd4308c2ed806cd8baf07c3
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-10-15 18:24:43 +0530

    Update RCA.sv

--- SystemVerilog/RCA.sv
+++ SystemVerilog/RCA.sv
@@ -4,8 +4,8 @@ module RCA(input logic (3:0)a, b,
 			  output logic cout);
 
 	FA A1 (a[0], b[0], cin, c[0], s[0]);
-	FA A2 (a[1], b[1], c[0], c[1], s[1]);
-	FA A3 (a[2], b[2], c[1], c[2], s[2]);
+	FA A2 (a[1], b[0], c[0], c[1], s[1]);
+	FA A3 (a[2], b[2], c[1], c[0], s[2]);
 	FA A4 (a[3], b[3], c[2], cout, s[3]);
 	end
 endmodule
