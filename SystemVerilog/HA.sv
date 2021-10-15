c92b76c66a0c02420cbe042da05378bcfc44afd4
Skanda-Prasad <92496389+Skanda-Prasad@users.noreply.github.com>
2021-10-15 18:41:06 +0530

    Update HA.sv

--- SystemVerilog/HA.sv
+++ SystemVerilog/HA.sv
@@ -3,5 +3,33 @@
 
 module HA (input logic in1, in2, output logic out, car);
 	assign out = in1 ^ in2;
-	assign car = in1 | in2;
+	assign car = in1 & in2;
+endmodule
+
+module HA_TESTBENCH;
+
+reg in1;
+
+reg in2;
+
+wire out;
+
+wire car;
+
+	ha uut ( .in1(in1),.in2(in2),.out(out), .car(car));
+
+initial begin
+
+#10 in1=1’in20;in2=1’in20;                       –This is input in1=0,in2=0
+
+#10 in1=1’in20;in2=1’in21;                        –This is input in1=0,in2=1
+
+#10 in1=1’in21;in2=1’in20;                        –This is input in1=1,in2=0
+
+#10 in1=1’in21;in2=1’in21;                         –This is input in1=1,in2=1
+
+#10$stop;
+
+end
+
 endmodule
