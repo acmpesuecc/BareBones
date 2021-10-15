e1a63e421f5fcc29acecfe6121c4e2b8f0120f1a
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-10-15 18:23:39 +0530

    Update and rename tbn.sv to HA.sv

--- SystemVerilog/HA.sv
+++ SystemVerilog/HA.sv
@@ -1,7 +1,7 @@
 //test SystemVerilog file
 //This is a half adder design using dataflow style of coding.
 
-module tbn (input logic in1, in2, output logic out, car);
+module HA (input logic in1, in2, output logic out, car);
 	assign out = in1 ^ in2;
 	assign car = in1 | in2;
 endmodule
